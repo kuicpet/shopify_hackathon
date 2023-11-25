e17fe6e2f5b5d72055ad0bfa1fb3e0f8d95b867b
kingsley umujeyan <kingsleyumujeyan@gmail.com>
2023-11-25 20:40:51 +0100

    ft--menu updated

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 // DOM Elements
 let notificationBtn = document.querySelector('.notify')
- let notification = document.querySelector('.notification')
+let notification = document.querySelector('.notification')
+let menu = document.querySelector('.menu')
 
 const toggleNotis = () => {
   notification.style.display =
     notification.style.display === 'none' || notification.style.display === ''
+      ? 'block'
+      : 'none'
+}
+
+const toggleMenu = () => {
+  menu.style.display =
+    menu.style.display === 'none' || menu.style.display === ''
       ? 'block'
       : 'none'
 }
